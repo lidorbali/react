dbc8c55845ffae3f4e19c20ad73a13db6f0515bd
lidorbali <99362432+lidorbali@users.noreply.github.com>
2022-08-16 00:48:40 +0300

    tirgul react 1 end

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import "./App.css";
 import MyNavbar from "./mynavbar";
 import MainContent from  "./mainContent"
 import Footer from "./footer";
+import { useState } from "react";
+
+
 function App() {
+ const [Counter, setCounter] = useState(1)
+ const [str, setstr] = useState("")
   return (
     <div className="App">
       <MyNavbar />
       <header className="App-header">
+      
+      <button onClick={()=>setCounter(Counter+1)}>{Counter}</button>
+      
+      <button onClick={()=>setstr(str+"d")}>{str}</button>
+  
       <MainContent/>
     
       </header> 
